9ea6964ad14aae2f596d3b7e9a656c9f0d7eb569
James OSullivan <james.a.osullivan01@gmail.com>
2023-09-16 17:09:34 -0500

    added getters

--- EmailApp/src/Email.java
+++ EmailApp/src/Email.java
@@ -83,6 +83,10 @@ public class Email {
         }
     }
 
+    public int getMailboxCapacity(){return mailboxCapacity;}
+    public String getAlternateEmail(){return alternateEmail;}
+    public String getPassword(){return password;}
+
 
 
     @Override
